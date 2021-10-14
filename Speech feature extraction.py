da793b826e707785a839c2b7a2a133536deb193c
su <2418170473@qq.com>
2021-10-15 01:48:06 +0800

    加窗和离散傅里叶变换完事了，用真正的语音看起来很懵逼，data里又添加了老师ppt里的音频，表现异常良好，因为他真的只有两种频率！

--- Speech feature extraction.py
+++ Speech feature extraction.py
@@ -1,11 +1,13 @@
 import wave as wv
 import pylab as plt
 import numpy as np
+import scipy.signal as signal
+from scipy.fftpack import fft,ifft
 
 
 def read_voice_signal():
-    '''从文件读取声音信号'''
-    wave_path = 'data/shan.wav'
+    """从文件读取声音信号"""
+    wave_path = 'data/media1.wav'
     file = wv.open(wave_path, 'rb')
 
     # a = file.getparams().nframes  # 采样总数
@@ -28,15 +30,30 @@ def read_voice_signal():
     wave_data.shape = -1, 2
     # 一行
     wave_data = wave_data.T
-    # 最后通过取样点数和取样频率计算出每个取样的时间：
+    # 最后通过取样点数和取样频率计算出每个取样的时间timelist
     # arange是在给定区间内均匀地返回值
     timelist = np.arange(0, nframes) * (1.0 / framerate)
-    return wave_data, timelist
+    return wave_data, timelist, framerate, nframes
 
 
-def image_display():
-    '''展示图像'''
-    wavdata, wavtime = read_voice_signal()
+def frequence_domain_display(fredata, num, f):
+    """展示频域图像"""
+    timelist = np.arange(0, num) * (f / num)
+
+    normalization_y = np.abs(fredata) / num
+    plt.plot(timelist, normalization_y, color='red')
+    plt.show()
+
+
+def time_domain_display(wavdata, num, f):
+    """展示时域图像"""
+    timelist = np.arange(0, num) * (1.0 / f)
+    plt.plot(timelist, wavdata, color='red')
+    plt.show()
+
+
+def image_display(wavdata, wavtime):
+    """展示图像"""
     plt.title("音频双声道显示")
     plt.subplot(211)
     plt.plot(wavtime, wavdata[0], color='red')
@@ -46,21 +63,92 @@ def image_display():
 
 
 def pre_emphasis(wavedata):
-    '''预加重'''
+    """预加重"""
     # 设置系数
     coefficient = 0.97
     # 通过map预加重
     new_wavedata = np.delete(wavedata[0], 0)
-    left_wavedata = np.array(map(lambda x, y: x-coefficient*y, wavedata[0], new_wavedata))
+    left_wavedata = np.array(map(lambda x, y: x - coefficient * y, wavedata[0], new_wavedata))
     new_wavedata = np.delete(wavedata[1], 0)
     right_wavedata = np.array(map(lambda x, y: x - coefficient * y, wavedata[1], new_wavedata))
     return left_wavedata, right_wavedata
 
+
+def windows(wavedata, timelist, f):
+    """分帧与加窗"""
+    # 每帧有多少个采样点
+    wlen = int(25 * f / 1000)
+    # 每次移动frame移动多少个采样点
+    inc = int(10 * f / 1000)
+    # 重叠部分
+    overlap = wlen - inc
+    # 算出frame的个数 注意这里一定要用某一个声道的求数量，不然用双声道的，他认为向量个数为2，我赣！
+    signal_length = len(wavedata[0])
+    if signal_length <= wlen:  # 若信号长度小于一个帧的长度，则帧数定义为1
+        nf = 1
+    else:  # 否则，计算帧的总长度
+        nf = int(np.ceil((1.0 * signal_length - wlen + inc) / inc))
+    print(nf)
+    # 所有帧加起来总的铺平后的长度
+    pad_length = int((nf - 1) * inc + wlen)
+    # 不够的长度使用0填补，类似于FFT中的扩充数组操作
+    zeros = np.zeros((pad_length - signal_length,))
+    # 填补后的信号记为pad_signal
+    pad_signal = np.concatenate((wavedata[0], zeros))
+    # 相当于对所有帧的时间点进行抽取，得到nf*wlen长度的矩阵
+    indices = np.tile(np.arange(0, wlen), (nf, 1)) + np.tile(np.arange(0, nf * inc, inc), (wlen, 1)).T
+    # print(indices)
+    # nf行，wlen列.每一行就是每一frame的取样点的序号，从零开始
+    # 将indices转化为矩阵
+    indices = np.array(indices, dtype=np.int32)
+    print(indices.shape)
+    # 得到帧信号,比如frames[0]就是第一个frame的25个量化值的向量
+    frames = pad_signal[indices]
+    # 调用汉明窗
+    windown = np.hanning(wlen)
+    # 创建一个新的numpyarray
+    frames_afterwindows = np.zeros((1, int(25 * f/1000)))
+    # print(frames_afterwindows)
+    # 对每一frame依次加窗
+    for i in frames:
+        i = i * windown
+        # print(i)
+        i = np.expand_dims(i, axis=0)  # 升维，才能进行下一步的拼接
+        # 把每一步的出来的经过windows的frame作为新的一行添加到frames_afterwindows里
+        frames_afterwindows = np.concatenate([frames_afterwindows, i])
+        # np.append(frames_afterwindows, i, axis=0)# axis=0 增加行，列数不变
+    # 把一开始为了创建一个向量用的玩意去掉
+    np.delete(frames_afterwindows, 0)
+    # 返回经过windows的frame的矩阵，每一frame的
+    print(frames_afterwindows)
+    return frames_afterwindows, wlen, nf
+
+
+def all_frames_fft(frames_afterwindows, f):
+    frames_afterfft = np.zeros((1, int(25 * f / 1000)))
+    for i in frames_afterwindows:
+        i = fft(i)
+        # print(i)
+        i = np.expand_dims(i, axis=0)  # 升维，才能进行下一步的拼接
+        # 把每一步的出来的经过windows的frame作为新的一行添加到frames_afterwindows里
+        frames_afterfft = np.concatenate([frames_afterfft, i])
+    np.delete(frames_afterfft, 0)
+    return frames_afterfft
+
+
+
+
 def main():
-    image_display()
+    wave_data, timelist, f, sum = read_voice_signal()
+    print(f)
+    # image_display()
+    frames_afterwindows, wlen, nf = windows(wave_data, timelist, f)
+    time_domain_display(frames_afterwindows[15], wlen, f)
+    frames_afterfft = all_frames_fft(frames_afterwindows, f)
+    # print(frames_afterfft[800])
+    frequence_domain_display(np.abs(frames_afterfft[130]), wlen, f)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
 
--- test.py
+++ test.py
@@ -2,13 +2,77 @@ import os
 import wave as wv
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft, ifft
+from matplotlib.pylab import mpl
 
 
 def main():
+    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+    mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
+
+    # 采样点选择1400个，因为设置的信号频率分量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，所以这里设置采样频率为1400赫兹（即一秒内有1400个采样点，一样意思的）
+    x = np.linspace(0, 1, 1400)
+
+    # 设置需要采样的信号，频率分量有200，400和600
+    y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
+
+    fft_y = fft(y)  # 快速傅里叶变换
+
+    N = 1400
+    x = np.arange(N)  # 频率个数
+    half_x = x[range(int(N / 2))]  # 取一半区间
+
+    abs_y = np.abs(fft_y)  # 取复数的绝对值，即复数的模(双边频谱)
+    angle_y = np.angle(fft_y)  # 取复数的角度
+    normalization_y = abs_y / N  # 归一化处理（双边频谱）
+    normalization_half_y = normalization_y[range(int(N / 2))]  # 由于对称性，只取一半区间（单边频谱）
+
+    plt.subplot(231)
+    plt.plot(x, y)
+    plt.title('原始波形')
+
+    plt.subplot(232)
+    plt.plot(x, fft_y, 'black')
+    plt.title('双边振幅谱(未求振幅绝对值)', fontsize=9, color='black')
+
+    plt.subplot(233)
+    plt.plot(x, abs_y, 'r')
+    plt.title('双边振幅谱(未归一化)', fontsize=9, color='red')
+
+    plt.subplot(234)
+    plt.plot(x, angle_y, 'violet')
+    plt.title('双边相位谱(未归一化)', fontsize=9, color='violet')
+
+    plt.subplot(235)
+    plt.plot(x, normalization_y, 'g')
+    plt.title('双边振幅谱(归一化)', fontsize=9, color='green')
+
+    plt.subplot(236)
+    plt.plot(half_x, normalization_half_y, 'blue')
+    plt.title('单边振幅谱(归一化)', fontsize=9, color='blue')
+
+    plt.show()
+
+    # print(np.array([1,2,3])*np.array([2,3,4]))
+    # # 帧长
+    # wlen = 25
+    # # 帧移
+    # inc = 10
+    # nf = 10
+    # indices = np.tile(np.arange(0, wlen), (nf, 1)) + np.tile(np.arange(0, nf * inc, inc), (wlen, 1)).T
+    # print(indices)
+    # # 将indices转化为矩阵
+    # indices = np.array(indices, dtype=np.int32)
+    # print(indices)
+    # # 得到帧信号
+    # # frames = pad_signal[indices]
+    # # 调用汉明窗
+    # windown = np.hanning(wlen)
+
     wave_path = 'data/shan.wav'
-    s = '12345678'
-    array = np.fromstring(s, dtype=np.short)
-    print(array)
+    # s = '12345678'
+    # array = np.fromstring(s, dtype=np.short)
+    # print(array)
 
     # 以二进制文件打开
     # file = open("shan.wav", "rb")
@@ -16,9 +80,10 @@ def main():
     # print(s)
 
     # 尝试getparams函数
-    # shanWav = wv.open('shan.wav', 'rb')
+    # shanWav = wv.open(wave_path, 'rb')
     # params = shanWav.getparams()
-    # print(params)
+    # print(params[2])
+    # windows(shanWav)
 
 '''
     file = wv.open(wave_path)
@@ -37,7 +102,29 @@ def main():
     plt.xlabel("time (s)")
     plt.show()
 '''
-
+def windows(wavedata):
+    """分帧与加窗"""
+    # 帧长
+    wlen = 25
+    # 帧移
+    inc = 10
+    # 重叠部分
+    overlap = wlen - inc
+    # 算出frame的个数
+    signal_length = len(wavedata)
+    if signal_length <= wlen:  # 若信号长度小于一个帧的长度，则帧数定义为1
+        nf = 1
+    else:  # 否则，计算帧的总长度
+        nf = int(np.ceil((1.0 * signal_length - wlen + inc) / inc))
+    # 所有帧加起来总的铺平后的长度
+    pad_length = int((nf - 1) * inc + wlen)
+    # 不够的长度使用0填补，类似于FFT中的扩充数组操作
+    zeros = np.zeros((pad_length - signal_length,))
+    # 填补后的信号记为pad_signal
+    pad_signal = np.concatenate((wavedata, zeros))
+    # 相当于对所有帧的时间点进行抽取，得到nf*wlen长度的矩阵
+    indices = np.tile(np.arange(0, wlen), (nf, 1)) + np.tile(np.arange(0, nf * inc, inc), (wlen, 1)).T
+    print(indices)
 
 if __name__ == '__main__':
     main()
