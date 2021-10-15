48f66256ce6f72c4d82918c7e0ddbab99149ed57
su <2418170473@qq.com>
2021-10-15 14:01:24 +0800

    改进了画图的函数，然后准确地改进了fft，完成了mel滤波

--- Speech feature extraction.py
+++ Speech feature extraction.py
@@ -7,7 +7,7 @@ from scipy.fftpack import fft,ifft
 
 def read_voice_signal():
     """从文件读取声音信号"""
-    wave_path = 'data/media1.wav'
+    wave_path = 'data/shan.wav'
     file = wv.open(wave_path, 'rb')
 
     # a = file.getparams().nframes  # 采样总数
@@ -36,30 +36,27 @@ def read_voice_signal():
     return wave_data, timelist, framerate, nframes
 
 
-def frequence_domain_display(fredata, num, f):
-    """展示频域图像"""
-    timelist = np.arange(0, num) * (f / num)
-
-    normalization_y = np.abs(fredata) / num
-    plt.plot(timelist, normalization_y, color='red')
+def image_display(data, timelist):
+    """展示任意图像"""
+    plt.plot(timelist, data, color='red')
     plt.show()
 
 
-def time_domain_display(wavdata, num, f):
-    """展示时域图像"""
-    timelist = np.arange(0, num) * (1.0 / f)
-    plt.plot(timelist, wavdata, color='red')
-    plt.show()
+# def time_domain_display(wavdata, num, f):
+#     """展示时域图像"""
+#     timelist = np.arange(0, num) * (1.0 / f)
+#     plt.plot(timelist, wavdata, color='red')
+#     plt.show()
 
 
-def image_display(wavdata, wavtime):
-    """展示图像"""
-    plt.title("音频双声道显示")
-    plt.subplot(211)
-    plt.plot(wavtime, wavdata[0], color='red')
-    plt.subplot(212)
-    plt.plot(wavtime, wavdata[1])
-    plt.show()
+# def image_display(wavdata, wavtime):
+#     """展示图像"""
+#     plt.title("音频双声道显示")
+#     plt.subplot(211)
+#     plt.plot(wavtime, wavdata[0], color='red')
+#     plt.subplot(212)
+#     plt.plot(wavtime, wavdata[1])
+#     plt.show()
 
 
 def pre_emphasis(wavedata):
@@ -101,6 +98,7 @@ def windows(wavedata, timelist, f):
     # nf行，wlen列.每一行就是每一frame的取样点的序号，从零开始
     # 将indices转化为矩阵
     indices = np.array(indices, dtype=np.int32)
+    print('（frame数，一frame点数）')
     print(indices.shape)
     # 得到帧信号,比如frames[0]就是第一个frame的25个量化值的向量
     frames = pad_signal[indices]
@@ -124,7 +122,8 @@ def windows(wavedata, timelist, f):
     return frames_afterwindows, wlen, nf
 
 
-def all_frames_fft(frames_afterwindows, f):
+def all_frames_fft(frames_afterwindows, num, f):
+    # windows后的图片，frame个数，频率
     frames_afterfft = np.zeros((1, int(25 * f / 1000)))
     for i in frames_afterwindows:
         i = fft(i)
@@ -133,20 +132,74 @@ def all_frames_fft(frames_afterwindows, f):
         # 把每一步的出来的经过windows的frame作为新的一行添加到frames_afterwindows里
         frames_afterfft = np.concatenate([frames_afterfft, i])
     np.delete(frames_afterfft, 0)
-    return frames_afterfft
+    # 按找基频的倍数，一直延伸到可接受的最高频率即采样频率，但是由于三角函数的一些关系，会使fft后的频域图像对称分布，而
+    # 高频那部分的数据是和低频对称的，高频那部分是假的！
+    '''能量谱的取一半是把每一帧的取一半，而不是把帧数取一半'''
+    timelist = np.arange(0, num) * (f / num)    # 从零频率到最高采样频率
+    half_timelist = timelist[range(int(num / 2))]  # 取一半区间
+    # 对原频率取模为能量谱
+    normalization_frames_afterabs = (np.abs(frames_afterfft))**2
+    print(len(normalization_frames_afterabs))
+    normalization_half_frames_afterabs = normalization_frames_afterabs[:,range(int(num / 2))]  # 由于对称性，只取一半区间（单边频谱
+    print(len(normalization_half_frames_afterabs))
+    # 返回值各个是什么看上面注释，太不好描述了
+    return frames_afterfft, timelist, normalization_half_frames_afterabs, half_timelist
 
 
+def melfilter(half_frames_afterabs, nf, f):
+    # fft后的数据，帧数，频率
+    # 滤波器个数
+    nfilt = 26
+    low_freq_mel = 0
+    high_freq_mel = (2595 * np.log10(1 + (f / 2) / 700))  # 把 Hz 变成 Mel
+    mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # 将梅尔刻度等间隔
+    hz_points = (700 * (10 ** (mel_points / 2595) - 1))  # 把 Mel 变成 Hz
+    # 每一个滤波器的中间值bin
+    bin = np.floor((nf + 1) * hz_points / f)
+    # 每一个半能量谱的滤波器
+    fbank = np.zeros((nfilt, int(np.floor(nf / 2))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m - 1])  # left
+        f_m = int(bin[m])  # center
+        f_m_plus = int(bin[m + 1])  # right
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    # print(fbank[0])
+    # 获得的是frame数*mel滤波器个数的矩阵，每一行就是该帧的mel-scale power spectrum
+    melscale_power_spectrum = np.dot(half_frames_afterabs[0:nf], fbank.T)
+    # print(filter_banks.shape)
+    # print(filter_banks)
+    # 不能算log(0),所以处理一下
+    melscale_power_spectrum = np.where(melscale_power_spectrum == 0, np.finfo(float).eps, melscale_power_spectrum)  # 数值稳定性
+    # print(filter_banks)
+    # 进行log处理
+    melscale_power_spectrum = 10 * np.log10(melscale_power_spectrum)  # dB
+    # print(filter_banks)
+    # melscale_power_spectrum -= (np.mean(melscale_power_spectrum, axis=0) + 1e-8)
+    # print(filter_banks)
+    return melscale_power_spectrum, nfilt
 
 
 def main():
+    num_of_testframe = 80
+    # 双声道数据，有数据的时间点，取样频率，取样个数
     wave_data, timelist, f, sum = read_voice_signal()
     print(f)
     # image_display()
+    # 加窗后的数据，每帧的采样点个数，帧数
     frames_afterwindows, wlen, nf = windows(wave_data, timelist, f)
-    time_domain_display(frames_afterwindows[15], wlen, f)
-    frames_afterfft = all_frames_fft(frames_afterwindows, f)
+    # 对于每一frame来说的有数据的时间点
+    timelist_time_frame = np.arange(0, wlen) * (1.0 / f)
+    image_display(frames_afterwindows[num_of_testframe], timelist_time_frame)
+    # fft后的数据,及其平方并取前半个图的能量数据
+    frames_afterfft, timelist, normalization_half_frames_afterabs, timelist_fre_frame = all_frames_fft(frames_afterwindows, wlen, f)
     # print(frames_afterfft[800])
-    frequence_domain_display(np.abs(frames_afterfft[130]), wlen, f)
+    image_display(normalization_half_frames_afterabs[num_of_testframe], timelist_fre_frame)
+    melscale_power_spectrum, nfilt = melfilter(normalization_half_frames_afterabs, wlen, f)
+    # 高频的那部分降下去很正常，因为高频就没什么能量
+    image_display(melscale_power_spectrum[num_of_testframe], np.arange(0, nfilt))
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -7,51 +7,68 @@ from matplotlib.pylab import mpl
 
 
 def main():
-    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-    mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
+    # fs = 44100
+    # high_freq_mel = (2595 * np.log10(1 + (fs / 2) / 700))  # 把 Hz 变成 Mel
+    # mel_points = np.linspace(0, high_freq_mel, 26 + 2)  # 将梅尔刻度等间隔
+    # hz_points = (700 * (10 ** (mel_points / 2595) - 1))  # 把 Mel 变成 Hz
+    # bin = np.floor((1024 + 1) * hz_points / fs)
+    # print(bin)
+    # fbank = np.zeros((24, int(np.floor(149 / 2 + 1))))
+    # print(fbank.shape)
 
-    # 采样点选择1400个，因为设置的信号频率分量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，所以这里设置采样频率为1400赫兹（即一秒内有1400个采样点，一样意思的）
-    x = np.linspace(0, 1, 1400)
+    n = np.array([1, 2])
+    print(range(int(10 / 2)))
 
-    # 设置需要采样的信号，频率分量有200，400和600
-    y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
+    # n = np.array([complex(1, 2), complex(2, 3)])
+    # m = np.abs(n)
+    # print(n)
+    # print(m)
 
-    fft_y = fft(y)  # 快速傅里叶变换
-
-    N = 1400
-    x = np.arange(N)  # 频率个数
-    half_x = x[range(int(N / 2))]  # 取一半区间
-
-    abs_y = np.abs(fft_y)  # 取复数的绝对值，即复数的模(双边频谱)
-    angle_y = np.angle(fft_y)  # 取复数的角度
-    normalization_y = abs_y / N  # 归一化处理（双边频谱）
-    normalization_half_y = normalization_y[range(int(N / 2))]  # 由于对称性，只取一半区间（单边频谱）
-
-    plt.subplot(231)
-    plt.plot(x, y)
-    plt.title('原始波形')
-
-    plt.subplot(232)
-    plt.plot(x, fft_y, 'black')
-    plt.title('双边振幅谱(未求振幅绝对值)', fontsize=9, color='black')
-
-    plt.subplot(233)
-    plt.plot(x, abs_y, 'r')
-    plt.title('双边振幅谱(未归一化)', fontsize=9, color='red')
-
-    plt.subplot(234)
-    plt.plot(x, angle_y, 'violet')
-    plt.title('双边相位谱(未归一化)', fontsize=9, color='violet')
-
-    plt.subplot(235)
-    plt.plot(x, normalization_y, 'g')
-    plt.title('双边振幅谱(归一化)', fontsize=9, color='green')
-
-    plt.subplot(236)
-    plt.plot(half_x, normalization_half_y, 'blue')
-    plt.title('单边振幅谱(归一化)', fontsize=9, color='blue')
-
-    plt.show()
+    # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+    # mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
+    #
+    # # 采样点选择1400个，因为设置的信号频率分量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，所以这里设置采样频率为1400赫兹（即一秒内有1400个采样点，一样意思的）
+    # x = np.linspace(0, 1, 1400)
+    #
+    # # 设置需要采样的信号，频率分量有200，400和600
+    # y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
+    #
+    # fft_y = fft(y)  # 快速傅里叶变换
+    #
+    # N = 1400
+    # x = np.arange(N)  # 频率个数
+    # half_x = x[range(int(N / 2))]  # 取一半区间
+    #
+    # abs_y = np.abs(fft_y)  # 取复数的绝对值，即复数的模(双边频谱)
+    # angle_y = np.angle(fft_y)  # 取复数的角度
+    # normalization_y = abs_y / N  # 归一化处理（双边频谱）
+    # normalization_half_y = normalization_y[range(int(N / 2))]  # 由于对称性，只取一半区间（单边频谱）
+    #
+    # plt.subplot(231)
+    # plt.plot(x, y)
+    # plt.title('原始波形')
+    #
+    # plt.subplot(232)
+    # plt.plot(x, fft_y, 'black')
+    # plt.title('双边振幅谱(未求振幅绝对值)', fontsize=9, color='black')
+    #
+    # plt.subplot(233)
+    # plt.plot(x, abs_y, 'r')
+    # plt.title('双边振幅谱(未归一化)', fontsize=9, color='red')
+    #
+    # plt.subplot(234)
+    # plt.plot(x, angle_y, 'violet')
+    # plt.title('双边相位谱(未归一化)', fontsize=9, color='violet')
+    #
+    # plt.subplot(235)
+    # plt.plot(x, normalization_y, 'g')
+    # plt.title('双边振幅谱(归一化)', fontsize=9, color='green')
+    #
+    # plt.subplot(236)
+    # plt.plot(half_x, normalization_half_y, 'blue')
+    # plt.title('单边振幅谱(归一化)', fontsize=9, color='blue')
+    #
+    # plt.show()
 
     # print(np.array([1,2,3])*np.array([2,3,4]))
     # # 帧长
